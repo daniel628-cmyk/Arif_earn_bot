4102dfcbe6aa15e3fd5e7784cb7451edd090e545
daniel628-cmyk <aregad545@gmail.com>
2026-06-27 21:03:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ from database import connect_db, init_db
 from handlers.start import router as start_router
 from handlers.menu import router as menu_router
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
 dp.include_router(menu_router)
 
+
 async def main():
-    await connect_db()
-    await init_db()
+    connect_db()
+    init_db()
 
     print("🚀 Bot is running")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
