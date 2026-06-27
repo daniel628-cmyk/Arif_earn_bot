a8f95d725c9350e38e00e7aecc58d2e1d90cb80d
daniel628-cmyk <aregad545@gmail.com>
2026-06-27 20:05:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 from database import connect_db, init_db
+from handlers.start import router as start_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message()
-async def echo(msg):
-    await msg.answer("Bot is working 🚀")
+# register routers
+dp.include_router(start_router)
 
 async def main():
     await connect_db()
     await init_db()
 
-    print("🚀 Bot started")
+    print("🚀 Bot is running")
 
     await dp.start_polling(bot)
 
