3574e67c00a9e12702b3b10679532d9a56532151
daniel628-cmyk <aregad545@gmail.com>
2026-06-28 23:50:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ from database import connect_db, init_db
 
 # የHandlers ፋይሎችን ከዚህ አስገባ
 from handlers.start import router as start_router
-from handlers.channel_handler import router as channel_router # ይህንን አዲስ ፋይል አገናኘነው
+from handlers.channel_handler import router as channel_router
+from handlers.bot_handler import router as bot_router  # <--- አዲሱ የቦት ሃንድለር
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 # Router-ዎችን መመዝገብ
 dp.include_router(start_router)
-dp.include_router(channel_router) # ይህንን ጨምር
+dp.include_router(channel_router)
+dp.include_router(bot_router)  # <--- አዲሱን ራውተር እዚህ መመዝገብ አለብህ
 
 async def main():
     # 1. ዳታቤዝ ማገናኘት
     connect_db()
     
-    # 2. ሰንጠረዦችን መፍጠር (እዚህ ጋር ነው channels table የሚፈጠረው)
+    # 2. ሰንጠረዦችን መፍጠር (እዚህ ጋር ነው users, channels እና bots table የሚፈጠሩት)
     init_db()
 
     print("🚀 Bot is running smoothly...")
