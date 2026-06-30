26c70a4782c5d528fe22d597e2321845e3b9c80b
daniel628-cmyk <aregad545@gmail.com>
2026-06-30 21:24:15 +0300

    Update balance.py

--- handlers/balance.py
+++ handlers/balance.py
@@ -9,18 +9,11 @@ async def check_balance(message: Message):
     user_id = message.from_user.id
     
     conn = get_db()
-    with conn.cursor() as cur:
-        # ባላንሱን ፈልግ
-        cur.execute("SELECT amount FROM balances WHERE user_id = %s", (user_id,))
-        res = cur.fetchone()
-        
-        # ተጠቃሚው በዳታቤዝ ከሌለ 0 ይጀምር
-        if not res:
-            cur.execute("INSERT INTO balances (user_id, amount) VALUES (%s, 0)", (user_id,))
-            conn.commit()
-            amount = 0
-        else:
-            amount = res[0]
-    conn.close()
-    
-    await message.answer(f"💰 የአሁኑ ባላንስዎ፡ {amount} ብር ነው")
+    try:
+        with conn.cursor() as cur:
+            # መጀመሪያ ተጠቃሚው መኖሩን ቼክ እናድርግ
+            cur.execute("SELECT amount FROM balances WHERE user_id = %s", (user_id,))
+            res = cur.fetchone()
+            
+            if res:
+                amount = res[0]
