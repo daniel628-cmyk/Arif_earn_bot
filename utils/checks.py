cb4d4ab3cfed376bb7e230dab1035ec45c77964e
daniel628-cmyk <aregad545@gmail.com>
2026-06-30 19:13:20 +0300

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,1 +1,10 @@
-#
+# utils/checks.py
+async def check_user_sub(bot, chat_id, user_id):
+    try:
+        member = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+        # ተጠቃሚው ቻናሉ ውስጥ መሆኑን ያረጋግጣል
+        if member.status in ['member', 'administrator', 'creator']:
+            return True
+        return False
+    except Exception:
+        return False
