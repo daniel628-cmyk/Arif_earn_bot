c13b1d88291d2b311228bcb66484692eee0e744e
daniel628-cmyk <aregad545@gmail.com>
2026-06-30 12:43:07 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,30 +1,12 @@
-from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram import Router, F
 from aiogram.types import Message
-
-from db import add_user
 from keyboards.main_menu import main_menu
 
 router = Router()
 
-
-@router.message(CommandStart())
-async def start_handler(message: Message):
-
-    user_id = message.from_user.id
-    username = message.from_user.username or ""
-
-    add_user(user_id, username)
-
-    text = f"""
-👋 Welcome {message.from_user.first_name}
-
-💰 Earn money by completing simple tasks.
-
-Choose one of the options below.
-"""
-
+@router.message(F.text == "/start")
+async def start(message: Message):
     await message.answer(
-        text,
+        "Welcome to Earn Bot 🚀",
         reply_markup=main_menu
     )
