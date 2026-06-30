ccadb181bc2faefe77249c2595ac47d8b7efe9f7
daniel628-cmyk <aregad545@gmail.com>
2026-06-30 19:53:16 +0300

    Update withdraw.py

--- handlers/withdraw.py
+++ handlers/withdraw.py
@@ -1,1 +1,11 @@
-#
+from aiogram import Router, F
+from aiogram.types import Message
+
+# ይህ መስመር የግድ መኖር አለበት!
+router = Router() 
+
+# ከዚህ በታች ያሉትን የ Handler ኮዶችህን በ router.message ወይም router.callback_query መልክ ጻፋቸው።
+@router.message(F.text == "💸 Withdraw")
+async def withdraw_handler(message: Message):
+    # የ Withdraw ኮድህ እዚህ...
+    await message.answer("የሚወጣው ገንዘብ መጠን...")
