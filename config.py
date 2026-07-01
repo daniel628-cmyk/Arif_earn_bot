15886eb2f091cac554e0652d7ca8bcd8ca881e2d
daniel628-cmyk <aregad545@gmail.com>
2026-07-01 16:37:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "6422653762"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7439284276"))
