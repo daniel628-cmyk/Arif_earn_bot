56de2d924fc2ad4333a6bc0726b3e9cd90a64942
daniel628-cmyk <aregad545@gmail.com>
2026-07-04 13:14:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7439284276"))
+API_KEY = "CHANGE_THIS_TO_RANDOM_SECRET_KEY_123456"
