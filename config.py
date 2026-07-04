0890e6ca3e2b67c34c556e1ca716e3cf9534852b
daniel628-cmyk <aregad545@gmail.com>
2026-07-04 21:07:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7439284276"))
-API_KEY = "ArifEarn2026Secret"
+ADMIN_USERNAME = "Ariff_Support"
